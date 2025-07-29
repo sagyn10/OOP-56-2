945c778392dc304436edbf162f277aacc4c3b781
sagyn10-ber <sagynbaevaman7.09@gmail.com>
2025-07-29 16:38:03 +0600

    сделана 6 домашака

---
+++ homeworks/homework__6.py
@@ -0,0 +1,12 @@
+from blessed import Terminal
+
+term = Terminal()
+
+print(term.red("🍎 Apple"))
+print(term.yellow("🍊 Orange"))
+print(term.yellow("🍌 Banana"))
+print(term.green("🥝 Kiwi"))
+print(term.blue("🫐 Blueberry"))
+print(term.magenta("🍇 Grape"))
+print(term.cyan("🍈 Melon"))
+
